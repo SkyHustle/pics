ea4d1ba026f080506c4ec2fd7d46ce5b716878ab
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-12 13:24:39 -1000

    Create and configure axios instance as a helper in a separate folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
 
   // Cleaner Syntax
-  async onSearchSubmit(term) {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`
-      }
+  onSearchSubmit = async term => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
     });
     this.setState({ images: response.data.results });
-  }
+  };
 
   render() {
     return (
