b03e7304873bfc0392f1e4cca0253786978c2d68
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-12 12:42:28 -1000

    Convert to async function

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,29 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    // CORS Request
-    axios.get("https://api.unsplash.com/search/photos", {
+  // onSearchSubmit(term) {
+  //   // Async CORS Request that returns a Promise
+  //   axios
+  //     .get("https://api.unsplash.com/search/photos", {
+  //       params: { query: term },
+  //       headers: {
+  //         Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`
+  //       }
+  //     })
+  //     .then(response => {
+  //       console.log(response.data.results);
+  //     });
+  // }
+
+  // Cleaner Syntax
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`
       }
     });
+    console.log(response.data.results);
   }
 
   render() {
