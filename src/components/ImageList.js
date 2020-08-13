bd42e414a875fb7df15801d4e7fb55e2e24cd39b
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-12 15:38:50 -1000

    Display list of images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const ImageList = props => {
   console.log(props.images);
+  const images = props.images.map(image => {
+    return <img alt="" key={image.id} src={image.urls.regular} />;
+  });
 
-  return <div>ImageList</div>;
+  return <div>{images}</div>;
 };
 
 export default ImageList;
