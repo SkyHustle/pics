34f92c5b0618c80a6816c7c3d78a154426cb2845
Dmitry Sky <dmitry.sky@icloud.com>
2022-12-29 18:54:37 -0700

    Rewrite SearchBar class based component to funtional component using React Hook

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
 
-  onFormSubmit(event) {
+  function onFormSubmit(event) {
     event.preventDefault();
-    this.props.onSubmit(this.state.term);
+    onSubmit(term);
   }
-
-  render() {
-    return (
-      <div className="ui segment">
-        <form onSubmit={event => this.onFormSubmit(event)} className="ui form">
-          <div className="field">
-            <label>Image Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={event => this.setState({ term: event.target.value })}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
+  
+  return (
+    <div className="ui segment">
+      <form onSubmit={event => onFormSubmit(event)} className="ui form">
+        <div className="field">
+          <label>Image Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={event => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
